966192a35537b52d26ab365def3ebc049092a235
William de Castro <williamtdcastro@gmail.com>
2024-08-26 23:26:59 -0300

    chore: add base config of tailwind and add styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,44 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://thinglabs.tech',
+  integrations: [
+    starlight({
+      title: 'ThingLabs',
+      logo: {
+        src: './src/assets/thinglabs-logo.svg',
+        replacesTitle: true,
+      },
+      customCss: ['./src/tailwind.css'],
+      social: {
+        discord: 'https://tl.mt/d',
+        github: 'https://github.com/Car-Thing-Hax-Community',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: 'Example Guide',
+              slug: 'guides/example',
+            },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: {
+            directory: 'reference',
+          },
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
