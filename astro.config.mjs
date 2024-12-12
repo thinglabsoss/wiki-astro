72f478731c59d2aacbc27fc9a80db8c4c3802f19
SartorialGrunt0 <sunnygalli@gmail.com>
2024-12-12 11:26:46 -0800

    Fixed all the stuff from PR and alot of small things

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,23 +29,32 @@ export default defineConfig({
           ],
         },
         {
-          label: 'DeskThing',
+          label: 'ThingLabs Apps',
           items: [
             {
-              label: 'Introduction',
-              slug: 'deskthing/introduction',
-            },
-            {
-              label: 'DeskThing Server',
-              slug: 'deskthing/deskthing-server',
-            },
-            {
-              label: 'Managing Apps',
-              slug: 'deskthing/managing-apps',
-            },
-            {
-              label: 'Official Apps',
-              autogenerate: { directory: 'DeskThing/Apps' },
+              label: 'DeskThing',
+              items: [
+                {
+                  label: 'Introduction',
+                  slug: 'thinglabs-apps/deskthing/introduction',
+                },
+                {
+                  label: 'Server Configuration',
+                  slug: 'thinglabs-apps/deskthing/server-config',
+                },
+                {
+                  label: 'Managing Apps',
+                  slug: 'thinglabs-apps/deskthing/managing-apps',
+                },
+                {
+                  label: 'FAQ',
+                  slug: 'thinglabs-apps/deskthing/faq',
+                },
+                {
+                  label: 'Official Apps',
+                  autogenerate: { directory: '/ThingLabs-Apps/DeskThing/Apps' },
+                },
+              ]
             },
           ],
         },
@@ -63,15 +72,6 @@ export default defineConfig({
             {
               label: 'ADB Tools',
               slug: 'legacy/adb',
-            },
-          ],
-        },
-        {
-          label: 'Troubleshooting',
-          items: [
-            {
-              label: 'FAQ',
-              slug: 'troubleshooting/faq',
             },
           ],
         },
